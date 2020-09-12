63d6021224cf04016b20e617d485f585c5f4e150
Josh Johnson <josh.johnson@readynext.io>
2020-09-12 01:58:10 -0400

    Add Phone Number

--- src/Core/BodyGenesis.Core/Entities/Customer.cs
+++ src/Core/BodyGenesis.Core/Entities/Customer.cs
@@ -9,10 +9,12 @@ namespace BodyGenesis.Core.Entities
         public string Auth0UserId { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string EmailAddress { get; set; } = string.Empty;
-        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; } = string.Empty;
+        public DateTime? DateOfBirth { get; set; }
         public List<MembershipSubscription> MembershipSubscriptions { get; set; } = new List<MembershipSubscription>();
         public List<PaymentMethod> PaymentMethods { get; set; } = new List<PaymentMethod>();
-        public string PhoneNumber { get; set; } = string.Empty;
+
+        public bool CanStartMembership => !string.IsNullOrWhiteSpace(Name) && !string.IsNullOrWhiteSpace(EmailAddress) && DateOfBirth.HasValue && !string.IsNullOrWhiteSpace(PhoneNumber);
 
         public MembershipSubscription CurrentMembershipSubscription => MembershipSubscriptions.FirstOrDefault((subscription) =>
         {
--- src/Presentation/BodyGenesis.Presentation.Website/Pages/Membership/MyMembership.cshtml.cs
+++ src/Presentation/BodyGenesis.Presentation.Website/Pages/Membership/MyMembership.cshtml.cs
@@ -31,7 +31,10 @@ namespace BodyGenesis.Presentation.Website.Pages.Membership
         public string CustomerEmail { get; set; }
 
         [BindProperty]
-        public DateTime CustomerDateOfBirth { get; set; }
+        public DateTime? CustomerDateOfBirth { get; set; }
+
+        [BindProperty]
+        public string CustomerPhoneNumber { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
@@ -48,6 +51,7 @@ namespace BodyGenesis.Presentation.Website.Pages.Membership
             CustomerDateOfBirth = Customer.DateOfBirth;
             CustomerEmail = Customer.EmailAddress;
             CustomerName = Customer.Name;
+            CustomerPhoneNumber = Customer.PhoneNumber;
 
             if (Customer.CurrentMembershipSubscription != null && !Customer.CurrentMembershipSubscription.AgreementSigned)
             {
@@ -73,6 +77,7 @@ namespace BodyGenesis.Presentation.Website.Pages.Membership
             Customer.Name = CustomerName;
             Customer.EmailAddress = CustomerEmail;
             Customer.DateOfBirth = CustomerDateOfBirth;
+            Customer.PhoneNumber = CustomerPhoneNumber;
 
             await _mediator.Send(new SaveCustomerRequest(Customer));
         }
