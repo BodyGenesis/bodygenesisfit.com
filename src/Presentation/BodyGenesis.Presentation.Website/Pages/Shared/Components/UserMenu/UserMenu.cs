a47b3f0fe6de711f25ea07217fcb5879aa6da04b
Josh Johnson <josh.johnson@readynext.io>
2020-09-12 00:55:16 -0400

    Update Layout

---
+++ src/Presentation/BodyGenesis.Presentation.Website/Pages/Shared/Components/UserMenu/UserMenu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace BodyGenesis.Presentation.Website.Pages.Shared.Components.UserMenu
+{
+    public class UserMenu : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
